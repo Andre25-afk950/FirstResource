2e07f93b85fca4824878a9a637b0376c0d19f13f
Andre25-afk950 <boykeandrean25@gmail.com>
2025-11-20 03:13:37 +0700

    Add files via upload
    Signed-off-by: Andre25-afk950 <boykeandrean25@gmail.com>

---
+++ app.js
@@ -0,0 +1,77 @@
+let video = document.createElement("video");
+video.src="https://drive.google.com/uc?export=view&id=1DgNw88S2GDuZnUWY77ydKQkkB4j0bKIY";
+let createVideo = function(){
+  let box = document.getElementsByClassName("vidio");
+  
+  let gambar = new Image();
+  gambar.src="https://drive.google.com/uc?export=view&id=1FqBavQ62JiqONRh2sOOYNPJWyG71XVbL";
+  
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  let cWidth = canvas.width = 640;
+  let cHeight = canvas.height = 320;
+  canvas.setAttribute("class","draw")
+  box[0].append(canvas);
+  gambar.onload = function(){
+    ctx.drawImage(gambar,0,0,cWidth,cHeight);
+  }
+  video.addEventListener("play",dVideo)
+    
+  function dVideo(){
+    ctx.clearRect(0,0,cWidth,cHeight)
+    ctx.drawImage(video,0,0,cWidth,cHeight)
+    requestAnimationFrame(dVideo);
+  }
+}
+
+//setTitle
+let setTitle = {
+ title:"Zhao Lusi ~ Black Veil Bride",
+ init:function(){
+  let title = document.querySelector(".title");
+  title.textContent = this.title; 
+ }
+}
+
+// set lirik
+let setLrc = function(){
+  let box = document.querySelector(".lirik")
+  let xhr = new XMLHttpRequest();
+  
+  xhr.open("GET","lirik.txt",true);
+  xhr.send();
+  
+  xhr.onload = function(){
+   box.innerHTML = xhr.responseText;
+  }
+}
+function btnPlay(){
+    let box = document.getElementsByClassName("vidio");
+    let btn = document.createElement("canvas");
+    let ctxBtn = btn.getContext("2d");
+    let bWidth = btn.width = 75;
+    let bHeight = btn.height = 32;
+    
+    let playImage = new Image();
+    playImage.src = "https://drive.google.com/uc?export=view&id=1SIoMQRnJrcCFeHhojm1aN2HYnx31GGNM";
+    
+    box[0].append(btn);
+    btn.setAttribute("class","plyBtn")
+    
+    playImage.onload = function(){
+      ctxBtn.drawImage(playImage,0,0,bWidth,bHeight)
+    }
+    //play button
+    btn.addEventListener("click",()=>{
+    video.play();
+    btn.style.display="none";
+    })
+    
+  }
+function update(){
+  createVideo();
+  setLrc();
+  setTitle.init()
+  btnPlay();
+}
+update();
